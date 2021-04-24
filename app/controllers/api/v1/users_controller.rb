52ced78c2221b04a30255e958063bf08d40d0c02
Sam <samara.amanda01@gmail.com>
2021-04-24 09:42:54 -0400

    removed unused code

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,24 +1,7 @@
 class Api::V1::UsersController < ApplicationController
 
     def index
-         users = User.all
-         render json: UserSerializer.new(users)
+        users = User.all
+        render json: UserSerializer.new(users)
     end
-
-    # def create
-    #     user = User.new(user_params)
-    #     if user.save
-    #         render json: :user, status: :accepted
-    #         #allows us to send status code with our fetch request. 
-    #     else
-    #         render json: {errors: user.errors.full_messages}, status: :unprocessible_entity
-    #     end
-    # end
-
-    # private
-
-    # def user_params
-    #     params.require(:user).permit(:name, :email)
-    #     params.permit()
-    # end
 end
